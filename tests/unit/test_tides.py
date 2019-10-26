8ef1bf9d8d7e293811cedeed761f74b64660a718
Niall Grant <ngfgrant@gmail.com>
2019-10-26 21:27:40 +0100

    Add provider to Tides interface

--- tests/unit/test_tides.py
+++ tests/unit/test_tides.py
@@ -1,4 +1,5 @@
 import pytest
+from os import environ
 from mock import patch
 
 from tides import Tides
@@ -6,10 +7,12 @@ from tides.provider import TideProvider
 from tides.exceptions import MissingApiKeyError
 
 
-class TestTides(object):
+class TestTides:
     def test_missing_api_key_raises_missing_api_key_error(self):
+        key = environ.pop('TIDE_KEY')
         with pytest.raises(MissingApiKeyError):
             Tides('admiralty')
+        environ.update({"TIDE_KEY": key})
 
     @patch.dict('os.environ', {'TIDE_KEY': 'ABC123'})
     def test_tides_instantiates_with_api_key(self):
--- tides/__init__.py
+++ tides/__init__.py
@@ -4,12 +4,12 @@ from tides.provider import TideProvider
 from tides.exceptions import MissingApiKeyError
 
 
-class Tides(object):
+class Tides:
     def __init__(self, provider_name):
-        self.provider = provider_name
         self.TIDE_KEY = os.environ.get('TIDE_KEY', None)
         if self.TIDE_KEY is None:
             raise MissingApiKeyError("API Key for tide provider not set.")
+        self.provider = provider_name
 
     @property
     def provider(self):
@@ -17,4 +17,4 @@ class Tides(object):
 
     @provider.setter
     def provider(self, provider_name):
-        self.__provider = TideProvider.get_provider(provider_name)
+        self.__provider = TideProvider.get_provider(provider_name, self.TIDE_KEY)
